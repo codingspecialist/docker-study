a7d7391c426c0c0540dd387cb494d6ad02ddba5f
metacoding <getinthere@naver.com>
2023-08-17 18:52:28 +0900

    ex09 react, spring, db 코드 완료 - react 연결 남음

---
+++ ex09/db/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE product_tb (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50),
+  price INT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO product_tb (name, price) VALUES ('banana', 500);
+INSERT INTO product_tb (name, price) VALUES ('apple', 1000);
